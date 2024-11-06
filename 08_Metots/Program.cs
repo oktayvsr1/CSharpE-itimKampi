42887f1553bbbf785e67e96c90e83e967a660739
OKTAY AVŞAR <oktayavsar@stu.aydin.edu.tr>
2024-11-06 18:22:50 +0300

    xc

---
+++ 08_Metots/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Metots
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region void Metotlar
+            //void CustomerList()
+            //{
+            //    Console.WriteLine("Oktay Avşar");
+            //    Console.WriteLine("Mehmet Avşar");
+            //    Console.WriteLine("deniz Avşar");
+            //    Console.WriteLine("zilan Avşar");
+            //}
+            //CustomerList();
+
+            //void sum()
+            //{
+            //    int x = 1;
+            //    int y = 2;
+            //    int z = x+y;
+            //    Console.WriteLine(z);
+            //}
+            //sum();
+
+            #endregion
+
+            #region Geriye değer döndürmeyen parametreli metotlar
+
+            //void WriteMethod(string name)
+            //{
+            //    Console.WriteLine(name);
+            //}
+
+            //WriteMethod("Oktay");
+
+            //void CustomerCard(string name,string surnmae)
+            //{
+            //    Console.WriteLine("Müşteri: "+name+" "+surnmae);
+            //}
+            //CustomerCard("Oktay","Avşar");
+            #endregion
+
+            #region Geriye değer döndürmeyen ınt parametreli metotlar
+
+            //void sum(int s1,int s2,int s3)
+            //{
+            //    int result=s1 + s2+s3;
+            //    Console.WriteLine(result);
+            //}
+            //sum(2, 5, 7);
+            #endregion
+
+            #region geriye değer döndüren metotlar
+
+            //string CustomerName()
+            //{
+            //    return "Buse Yıldız";
+            //}
+
+            //Console.WriteLine(CustomerName());
+
+            //string StudentCard()
+            //{
+            //    string name = "Oktay";
+            //    string surname = "avşar";
+            //    return name+" " + surname;
+            //}
+            //Console.WriteLine(StudentCard());
+
+            #endregion
+
+            #region Geriye değer döndüren string parametreli metotlar
+
+            //string countrCard(string name,string capital,string flagColor)
+            //{
+            //    string cardInfo="Ülke "+name+" "+capital+" "+flagColor;
+            //    return cardInfo;
+            //}
+            //Console.Write("Ülke Adını giriniz: ");
+            //string x=Console.ReadLine();
+            //Console.Write("Ülke Başkenti giriniz: ");
+            //string y = Console.ReadLine();
+            //Console.Write("Ülke Bayrak rengini giriniz: ");
+            //string z = Console.ReadLine();
+
+            //Console.WriteLine(countrCard(x,y,z));
+
+
+            #endregion
+
+            #region geriye değer döndüren int parametreli metotlar
+
+            //int Sum(int s1,int s2)
+            //{
+            //    return s1 + s2;
+            //}
+            //Console.WriteLine(Sum(1,5));
+
+            #endregion
+
+            #region Örnek uygulama
+
+            //string ExamResult(string stu,int s1,int s2, int s3)
+            //{
+            //    int result = (s1 + s2 + s3)/3;
+            //    if (result >= 50)
+            //    {
+            //        return stu + " İsimli öğrenci sınavı geçti " + "Ortalama: " + result;
+            //    }
+            //    else
+            //    {
+            //        return stu + " İsimli öğrenci sınavı geçemedi " + "Ortalama: " + result;
+
+            //    }
+
+            //}
+            //Console.WriteLine(ExamResult("Oktay",55,25,45));
+
+            #endregion
+
+
+
+            Console.Read();
+        }
+    }
+}
---
+++ 09_DatabaseProject/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_DatabaseProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** C# veri tabanlı ürün-kategori bilgi sistemi ****");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            string tableNumber;
+            string bgl = @"Server=DESKTOP-7HJ6BE6\SQLEXPRESS01;Database=EgitimKampiDb;Trusted_Connection=true;TrustServerCertificate=true";
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("1-Kategoriler");
+            Console.WriteLine("2-Ürünler");
+            Console.WriteLine("3-Siparişler");
+            Console.WriteLine("4-Çıkış Yap");
+            Console.Write("Lütfen getirmek istediğiniz tablo numarasını giriniz: ");
+            tableNumber = Console.ReadLine(); 
+            Console.WriteLine("------------------------------------");
+            
+            SqlConnection con= new SqlConnection(bgl);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select * from categories",con);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            con.Close();
+
+            foreach(DataRow row in dt.Rows)
+            {
+                 
+               foreach(var item in row.ItemArray)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+
+            Console.Read();
+
+        }
+    }
+}
